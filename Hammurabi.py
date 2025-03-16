56f3ebbc71c718abc2b826d8c5e1137cfb0b6143
jwheller0013 <jwheller0013@yahoo.com>
2025-03-15 20:31:23 -0400

    Starts, increases years, buys/sells

--- Hammurabi.py
+++ Hammurabi.py
@@ -8,30 +8,16 @@ class Hammurabi:
         self.playGame()
 
     def playGame(self):
-        print ("Congratulations, you are the newest ruler of ancient Sumer, elected for a ten year term of " +
-               "office. Your \nduties are to dispense food, direct farming, and buy and sell land as " +
-               "needed to support your people.\n" + "Watch out for rat infestiations and the plague! " +
-               "Grain is the general currency, measured in bushels. The \n" + "following will help you in " +
-               "your decisions:\n\n" + "Each person needs at least 20 bushels of grain per year to survive\n" +
-               "Each person can farm at most 10 acres of land\n" +
-               "It takes 2 bushels of grain to farm an acre of land\n" +
-               "The market price for land fluctuates yearly\n\n" + "Rule wisely and you will be showered " +
-               "with appreciation at the end of your term.\n" + "Rule poorly and you will be kicked out of office! \n")
-
-        play_game =input("Are you ready to see if you can live up to being the great\n" + "Hammurabi?\n")
-        if play_game == "No":
-            print("So be it.")
-            play_game = False
-        if play_game == "Yes":
-            play_game = True
-            return play_game
-        else:
-            print("Nah you opened me to play a game not make Jim error code all your mistakes.")
-            play_game = False
-        while play_game == True:
+        Hammurabi.intro()
+        play_game = True
+        play_game = Hammurabi.start_game(play_game)
 
         # declare local variables here: grain, population, etc.
         # statements go after the declarations
+        if play_game == False:
+            print("Goodbye.")
+        while play_game == True:
+
             people = 100
             grain = 2800
             land = 19
@@ -41,29 +27,33 @@ class Hammurabi:
             harvest = 3000
             bushelsPerAcre = 3
             ratsDestroyed = 200
-            year = 1
             planted_land = 0
+            bought_land = 0
+            sold_land = 0
+            buy = True
 
-            def printSummary():
+            for i in range(1, 11, 1):
                 print("O great Hammurabi!\n" +
-                    "You are in year " + str(year) + " of your ten year rule.\n" +
-                    "In the previous year " + str(starved) + " people starved to death.\n" +
-                    "In the previous year " + str(immigrants) + " people entered the kingdom.\n" +
-                    "The population is now " + str(people) + ".\n" +
-                    "We harvested " + str(harvest) + " bushels at " + str(bushelsPerAcre) + " bushels per acre.\n" +
-                    "Rats destroyed " + str(ratsDestroyed) + " bushels, leaving " + str(grain) + " bushels in storage.\n" +
-                    "The city owns " + str(land) + " acres of land.\n" +
-                    "Land is currently worth " + str(land_value) + " bushels per acre.")
+                "You are in year " + str(i) + " of your ten year rule.\n" +
+                "In the previous year " + str(starved) + " people starved to death.\n" +
+                "In the previous year " + str(immigrants) + " people entered the kingdom.\n" +
+                "The population is now " + str(people) + ".\n" +
+                "We harvested " + str(harvest) + " bushels at " + str(bushelsPerAcre) + " bushels per acre.\n" +
+                "Rats destroyed " + str(ratsDestroyed) + " bushels, leaving " + str(grain) + " bushels in storage.\n" +
+                "The city owns " + str(land) + " acres of land.\n" +
+                "Land is currently worth " + str(land_value) + " bushels per acre.")
 
-            printSummary()
-            land = int(land) + int(Hammurabi.askHowManyAcresToBuy(grain, land_value))
-            grain = int(grain) - int(land) * int(land_value)
-            land = int(land) - int(Hammurabi.askHowManyAcresToSell(land))
-            grain = int(grain) + int(land) * int(land_value)
-            grain = int(grain) - int(Hammurabi.askHowMuchGrainToFeedPeople(grain))
-            planted_land = int(Hammurabi.askHowManyAcresToPlant(land, people, grain))
-            printSummary()
-            print (planted_land)
+                Hammurabi.askBuyOrSell(buy)
+                if buy == True:
+                    bought_land = Hammurabi.askHowManyAcresToBuy(grain,land_value)
+                if buy == False:
+                    sold_land = Hammurabi.askHowManyAcresToSell(land)
+                land = int(land) - int(sold_land) + int(bought_land)
+                grain = int(grain) + (int(sold_land) - int(bought_land)) * int(land_value)
+                #this handles buying and selling
+
+                grain = int(grain) - int(Hammurabi.askHowMuchGrainToFeedPeople(grain))
+                planted_land = int(Hammurabi.askHowManyAcresToPlant(land, people, grain))
 
 
 
@@ -76,6 +66,30 @@ class Hammurabi:
     #     x = int(people) - 10
     #     people = x
     #     return people
+
+    def start_game(x):
+        z =input("Are you ready to see if you can live up to being the great\n" + "Hammurabi?\n")
+        if z == "No":
+            print("So be it.")
+            x = False
+        if z == "Yes":
+            x = True
+        else:
+            print("Nah you opened me to play a game not make Jim error code all your mistakes.")
+            x = False
+        return x
+
+
+    def intro():
+        print ("Congratulations, you are the newest ruler of ancient Sumer, elected for a ten year term of " +
+               "office. Your \nduties are to dispense food, direct farming, and buy and sell land as " +
+               "needed to support your people.\n" + "Watch out for rat infestiations and the plague! " +
+               "Grain is the general currency, measured in bushels. The \n" + "following will help you in " +
+               "your decisions:\n\n" + "Each person needs at least 20 bushels of grain per year to survive\n" +
+               "Each person can farm at most 10 acres of land\n" +
+               "It takes 2 bushels of grain to farm an acre of land\n" +
+               "The market price for land fluctuates yearly\n\n" + "Rule wisely and you will be showered " +
+               "with appreciation at the end of your term.\n" + "Rule poorly and you will be kicked out of office! \n")
 
     def askHowManyAcresToBuy(grain,land_value):
         howMany = input("How many acres would you like to buy? \n")
@@ -93,6 +107,13 @@ class Hammurabi:
         return howMany
 
     #need to combine these
+    def askBuyOrSell (buy):
+        buyOrSell = input("So my lord are we buying or selling land?\n")
+        if buyOrSell == "buying":
+            buy = True
+        if buyOrSell == "selling":
+            buy = False
+        return buy
 
     def askHowMuchGrainToFeedPeople (grain):
         howMany = input("How much grain shall we provide the people with? \n")
